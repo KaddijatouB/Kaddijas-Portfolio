22d86a10f8a1507cb0ccbc21f35ad3508aaf5bcd
Kaddijatou Baldeh <baldehk@spu.edu>
2022-03-30 20:18:56 -0700

    social icons to my site

--- script.js
+++ script.js
@@ -20,84 +20,67 @@ function showDivs(n) {
   x[slide-1].style.display = "block"; 
 }
 
-var userName = window.prompt("Hello, What is your name? ");
-console.log("Let's play", userName);
 
-var MAX_TRY = 5;
-var numAttempts = 1;
+var questions = [{
+    question: "What is Kaddijatou's favorite color?",
+    answers: ["Black", "Pink"]
+}, {
+    question: "What is Kaddijatou's millitary occupation?",
+    answers: ["Medic", "Healthcare specialist", "68W"]
+}, {
+    question: "What is Kaddijatou's favorite bible story",
+    answers: ["Ruth", "Paul", "David"]
+}];
 
-
-while (numAttempts <= MAX_TRY){
-    print("Attempt", numAttempts)
-    userGuesses = int(input("Enter a guess:"))
-    computerGuesses = randint(MIN,MAX)
-    numAttempts = numAttempts + 1
-    if (userGuesses == computerGuesses){
-        print("Good Job,",userName,"You guessed my number in",numAttempts, "guesses!")
-        break;
+// Generic function to return a shuffled array:
+function shuffled(arr) {
+    arr = arr.slice(); 
+    for (var i = 0; i < arr.length; i++) {
+        var j = Math.floor(Math.random() * (arr.length - i)) + i;
+        [arr[i], arr[j]] = [arr[j], arr[i]]; // swap
     }
-    else if (userGuesses < MIN){
-        print("You must enter a number between 1 and 20")
-    }
-    else if (userGuesses > MAX){
-        print("You must enter a number between 1 and 20")
-    }
-    
-    else{
-        if (numAttempts > MAX_TRY){
-            print("Gameover!")
-            playAgain = input("Would you like to play again?")
-            if (playAgain == 'yes'){
-                numAttempts = 1
-            }else{
-                print("Thank you for playing")
-            }
-        }
-    }
-    
+    return arr;
 }
-  
-  
-  
-/**
- * userName = input("Hello, What is your name? ")
-print()
-print("Let's play", userName)
-print()
-print("Computer is thinking...")
-time.sleep(2)
-print()
 
-print(userName,"you have", MAX_TRY, "attempts to guess a number between 1 and 20", sep =' ')
-print()
+// define variables for some of the HTML elements:
+var domQuestion = document.querySelector('#question');
+var domAnswers = Array.from(document.querySelectorAll('input[name=answer]'));
+var domNext = document.querySelector('#next');
 
+function displayQuestion() {
+    // get a random order for the answers:
+    var answers = shuffled(questions[questionId].answers);
+    // Display question
+    domQuestion.textContent = (questionId+1) + '. ' + 
+                              questions[questionId].question;
+    domAnswers.forEach(function (input, i){
+        // Set checkbox value and unselect it
+        input.value = answers[i];
+        input.checked = false;
+        // Display the answer text
+        input.nextElementSibling.textContent = answers[i];
+    });
+}
+// Initialise and display first question
+var questionId = 0;
+var correctAnswers = 0;
+displayQuestion();
 
-numAttempts = 1
-
-
-while (numAttempts <= MAX_TRY):
-  print("Attempt", numAttempts)
-  userGuesses = int(input("Enter a guess:"))
-  computerGuesses = randint(MIN,MAX)
-  numAttempts = numAttempts + 1
-  
-  if (userGuesses == computerGuesses):
-    print("Good Job,",userName,"You guessed my number in",numAttempts, "guesses!")
-    break
-  elif (userGuesses < MIN):
-    print("You must enter a number between 1 and 20")
-  elif (userGuesses > MAX):
-    print("You must enter a number between 1 and 20")
-  elif (userGuesses < computerGuesses) and not (userGuesses < MIN):
-    print("Too low!")
-  elif (userGuesses > computerGuesses) and not (userGuesses > MAX):
-    print("Too high!")
-  if (numAttempts > MAX_TRY):
-    print("Gameover!")
-    playAgain = input("Would you like to play again?")
-    if playAgain == 'yes':
-      #reset game
-      numAttempts = 1
-    else:
-      print("Goodbye")
- */
+// Respond to a click on the Next button 
+domNext.addEventListener('click', function () {
+    // update correct answer counter:
+    var domAnswer = domAnswers.find(input => input.checked);
+    if (!domAnswer) return; // nothing was selected
+    // update number of correctly answered questions:
+    if (domAnswer.value == questions[questionId].answers[0]) correctAnswers++;
+    // next question
+    questionId++;
+    if (questionId >= questions.length) {
+        alert('You have answered ' + correctAnswers + 
+              ' of ' + questions.length + ' questions correctly.');
+        // restart
+        questionId = 0;
+        correctAnswers = 0;
+    }
+    displayQuestion();
+});
